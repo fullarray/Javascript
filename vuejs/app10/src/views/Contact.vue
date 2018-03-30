40b150f10dd9670a9f078d40e37e05a7df5cf467
Jonathan <sifreed77@gmail.com>
2018-03-30 01:17:07 -0400

    Added app10 implementation

---
+++ vuejs/app10/src/views/Contact.vue
@@ -0,0 +1,29 @@
+<template>
+   <div class="contact">
+       <div class="container">
+		<div class="row">
+			<div class="col-md-4 center">
+				<ContactComponent msg="left"/>
+			</div>
+			<div class="col-md-4 center">
+				<ContactComponent num="center"/>
+			</div>
+			<div class="col-md-4 center">
+				<ContactComponent yrd="right"/>
+			</div>
+		</div>
+      </div>
+   </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ContactComponent from '@/components/ContactComponent.vue'
+
+export default {
+  name: 'contact',
+  components: {
+    ContactComponent
+  }
+}
+</script>
