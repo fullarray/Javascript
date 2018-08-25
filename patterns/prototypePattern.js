11ad1e52680d4c2a274d89e9ddbb121f22cd4d5a
Jonathan <sifreed77@gmail.com>
2018-08-24 23:30:22 -0400

    Update prototypePattern.js

--- patterns/prototypePattern.js
+++ patterns/prototypePattern.js
@@ -3,11 +3,9 @@ var MazdaModels = function(){
 	this.manufacturer = 'Mazda';
 	this.make = 'Coupe';
 }
-
 MazdaModels.prototype.go = function(){
 	console.log("Car is moving forward.");
 }
-
 MazdaModels.prototype.stop = function(){
 	console.log("Car is stopped.");
 }
